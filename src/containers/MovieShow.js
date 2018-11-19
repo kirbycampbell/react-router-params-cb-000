2504c9d0443daf66f4101631371ebc8b972fa3d7
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-19 18:45:04 +0000

    automatically backed up by learn

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const MovieShow = props =>
+const MovieShow = ({ movie }) =>
   <div>
-    <h3>Movie Show Component!</h3>
+    <h3>Title: {movie.title}</h3>
   </div>
 
 const mapStateToProps = (state, ownProps) => {
