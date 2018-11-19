64f9b3e67437c40032ae0dc5da63eb7c2eefb2e1
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-19 18:46:04 +0000

    automatically backed up by learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -3,8 +3,9 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
+import MoviesNew from './MoviesNew';
 
-const MoviesPage = ({ match, movies }) => 
+const MoviesPage = ({ match, movies }) =>
   <div>
     <MoviesList movies={movies} />
     <Route path={`${match.url}/:movieId`} component={MovieShow}/>
